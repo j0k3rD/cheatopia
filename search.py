19205bd5917839096eb3852e0ce4c01ebbe83d4a
Aaron <agm.moya@alumno.um.edu.ar>
2023-05-20 22:05:20 -0300

    service fixed

--- search.py
+++ search.py
@@ -4,28 +4,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 
 
-
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.0.0'
 # Edge path for Linux
-edge_driver_path = os.path.join(os.getcwd(), '/home/j0k3r/Descargas/msedgedriver')
-edge_service = Service(edge_driver_path)
-edge_options = Options()
-edge_options.add_argument(f'user-agent={user_agent}')
+edge_service = Service("/home/j0k3r/Descargas/msedgedriver")
 
 # Configuración de Selenium y el navegador Firefox
-browser = webdriver.Edge(service=edge_service, options=edge_options)
+browser = webdriver.Edge(service=edge_service)
 
 # Realiza la búsqueda en https://chat.openai.com
 # def search():
 #Inicializa el navegador
-browser.get('https://bing.com')
-#Buscar por id
+browser.get("https://www.bing.com/ck/a?!&&p=c43bc6b638c064d5JmltdHM9MTY4NDU0MDgwMCZpZ3VpZD0zOTZhYTM0OC0zNTJkLTZjMmUtM2M0NS1iMDRlMzQ4ODZkNTAmaW5zaWQ9NTE4Nw&ptn=3&hsh=3&fclid=396aa348-352d-6c2e-3c45-b04e34886d50&psq=chat+bing&u=a1aHR0cHM6Ly9iaW5nLmNvbS9jaGF0&ntb=1")
+
 WebDriverWait(browser, 10)\
-    .until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'textarea#prompt-textarea'))).send_keys('2x2')
+    .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                       'button.tone-precise')))\
+                                        .click()
+
+WebDriverWait(browser, 10)\
+    .until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.main-container'))).click()
+
+WebDriverWait(browser, 10)\
+    .until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'label.text-input'))).send_keys('2x2')
 #Dar click en el boton
 WebDriverWait(browser, 10)\
     .until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'svg.absolute p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:bg-gray-100 enabled:dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent right-1 md:right-2 disabled:opacity-40'))).click()
